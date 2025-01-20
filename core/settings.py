507bc8142b0a897a579487b20ecc744603a2cdf8
Lofienjoyerr <dan-kol@bk.ru>
2025-01-20 19:51:05 +0300

    Updated static

--- core/settings.py
+++ core/settings.py
@@ -88,9 +88,10 @@ USE_I18N = True
 TIME_ZONE = 'Europe/Moscow'
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_URL = 'media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/"),]
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 EMAIL_HOST = 'smtp.yandex.ru'
