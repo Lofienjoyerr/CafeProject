08936d926018001d7373578fbd89b790ecb65033
Lofienjoyerr <dan-kol@bk.ru>
2025-02-04 20:33:25 +0300

    Formatted some code

--- users/services.py
+++ users/services.py
@@ -55,7 +55,7 @@ def get_password_active_tokens(user: User) -> QuerySet:
 
 def verify_email(token: str) -> bool:
     verify_tokens = EmailVerifyToken.objects.select_related('email_address').filter(token=token,
-                                                                                   created__gte=datetime.now() - EMAIL_CONFIRM_TIME)
+                                                                                    created__gte=datetime.now() - EMAIL_CONFIRM_TIME)
     if verify_tokens:
         verify_token = verify_tokens.first()
         verify_token.email_address.verified = True
